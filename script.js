583358e566f116eb08adc27527f14920f6e78624
varunkewlani <91694985+varunkewlani@users.noreply.github.com>
2024-07-26 23:05:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name, age)
+	{
+		this.getter=name
+		this.setter=age			
+	}
+	get name()
+	{
+		return this.name
+	}
+	set age()
+	{
+		this.age=
+	}
+		
+}
 
-class Student extends Person {}
+class Student extends Person {
+	study()
+	{
+		console.log(super.name(), "is studying")
+	}
+}
 
-class Teacher extends Person {}
+class Teacher extends Person {
+	teach()
+	{
+		console.log(super.name(), "is teaching);
+	}
+}
 
 // Do not change the code below this line
 window.Person = Person;
